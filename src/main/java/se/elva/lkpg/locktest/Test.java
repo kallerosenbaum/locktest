2948f3dfa9deaf473c3fbc4f0dfc91ef166d5e5d
Kalle Rosenbaum <kalle@rosenbaum.se>
2012-10-05 14:46:03 +0200

    Shelfing

--- src/main/java/se/elva/lkpg/locktest/Test.java
+++ src/main/java/se/elva/lkpg/locktest/Test.java
@@ -9,18 +9,26 @@ public class Test
 	public static void main(String[] args) throws Exception
 	{
 		EmbeddedCacheManager manager = new DefaultCacheManager("infinispan.xml");
-		Cache<Object, Object> cache = manager.getCache("default-dist-cache-works");
+		Cache<String, String> cache = manager.getCache("transactional-pessimistic-dist-sync-cache-fails");
 		String nodeid = System.getProperty("nodeid");
-
+		
+		if (!nodeid.equals("1")) {
+			for (;;) {
+				System.out.println("Doing nothing!");
+				Thread.sleep(30000);
+			}
+		}
+		
 		for (;;) {
+			long now = System.nanoTime();
 			if (cache.get("EXIT") != null) {
 				System.out.println("Cache told me to exit");
 				System.exit(1);
 			}
-			if (null == cache.putIfAbsent("lock", nodeid)) {
+			if (null == cache.putIfAbsent("lock", nodeid + " " + now)) {
 				// Now we should be alone!
-				Object lockOwner = cache.remove("lock");
-				if (!nodeid.equals(lockOwner)) {
+				String lockOwner = cache.remove("lock");
+				if (lockOwner == null || !nodeid.equals(lockOwner.substring(0, 1))) {
 					System.out.println("Wrong lock owner while unlocking. I'm " + nodeid + " owner was " + lockOwner);
 					System.out.println("Telling all nodes to exit");
 					cache.put("EXIT", "");
